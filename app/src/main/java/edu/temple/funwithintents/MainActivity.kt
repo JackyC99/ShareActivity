7ce667759a6686ed7d4aa2f638d11572ad0fcd06
JackyC99 <chenjacky838@gmail.com>
2025-04-01 11:58:21 -0400

    Added a share feature to the button so that it gave us options of where to share when button is clicked

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,6 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val textToShare = editText.text.toString()
+            if (textToShare.isNotEmpty()) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, textToShare)
+                    type = "text/plain"
+                }
+                startActivity(intent)
+            }
+
 
         }
     }
